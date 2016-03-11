198cee17834924527c54efbe9032858bf4dcecdb
EugeneHerasymchuk <evhenii.herasymchuk@gmail.com>
2016-03-11 16:30:46 +0200

    Added files via upload
    upload new files

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by EUGENE on 10.03.2016.
+ */
+//server side of project
+var app = require('express')(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server);
+
+server.listen(8080, function(){
+    console.log("server is listening");
+});
+
+io.on('connection', function(socket){
+    socket.on("xyz", function(x,y){
+        console.log(x+"   "+y);
+    });
+});
