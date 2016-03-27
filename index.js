5dda4d560ec0434f3f081d047d7261c9c430f4df
EugeneHerasymchuk <evhenii.herasymchuk@gmail.com>
2016-03-27 14:40:19 +0300

    Update index.js
    Add "var" due to "node-style" declaration

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
  * Created by EUGENE on 10.03.2016.
  */
 //server side of project
-var app = require('express')(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server);
+var app = require('express')();
+var server = require('http').Server(app),
+var io = require('socket.io')(server),
+var path = require('path');
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 server.listen(8080, function(){
