903bec8287b9f8c1ad9e0bb925aad78257cf8a1b
EugeneHerasymchuk <evhenii.herasymchuk@gmail.com>
2016-03-27 14:42:56 +0300

    Update index.js
    Add new event emitters.

--- index.js
+++ index.js
@@ -8,12 +8,39 @@ var io = require('socket.io')(server),
 var path = require('path');
 
 app.use(express.static(path.resolve(__dirname, 'client')));
-server.listen(8080, function(){
-    console.log("server is listening");
-});
+// variables to separate sockets from android and web-page
+var display,
+android;
 
+// socket connection
 io.on('connection', function(socket){
-    socket.on("xyz", function(x,y){
+	socket.on("android", function(data){
+	   	// if web-page connected
+	   	if(data === 0){ 
+      display = socket;
+      console.log("conected web-page");
+    }
+    else{
+      android = socket;
+      console.log("conected client");
+    }
+  });
+     // an event from client with coordinates of accelerometer
+socket.on("xyz", function(x,y){
+     // send these coordinates to index.html
+	display.emit("getTime", x, y);
         console.log(x+"   "+y);
     });
+    socket.on("keys", function(upS,downS,rightS,leftS){
+     // send these coordinates to index.html
+	display.emit("sendKeys",upS,downS,rightS,leftS);
+        console.log(upS,downS,rightS,leftS);
+    });
+    
 });
+
+
+server.listen(8080, function(){
+    console.log("server is listening on 8080");
+});
+
